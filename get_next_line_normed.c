b40c2b0f6670fec57bbc86a65bb6aef83aba3822
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2024-01-26 14:02:12 +0100

    normate le cose principali
    mancano alcuni controlli soprattutto verso la fine del file, e verificare con lo stdin invece che file

--- get_next_line_normed.c
+++ get_next_line_normed.c
@@ -4,47 +4,8 @@
 #include <fcntl.h>
 #include "libft.h"
 
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 100
 
-char	*ft_strrchr(const char *str, int c)
-{
-	int	len;
-
-	len = ft_strlen(str);
-	while (len >= 0)
-	{
-		if (str[len] == c)
-			return ((char *)&str[len]);
-		else
-			len--;
-	}
-	return (NULL);
-}
-void	ft_bzero(void *s, size_t n)
-{
-	size_t			i;
-	unsigned char	*temp;
-
-	i = 0;
-	temp = (unsigned char *)s;
-	while (i < n)
-	{
-		temp[i] = '\0';
-		i++;
-	}
-}
-void	*ft_calloc(size_t count, size_t size)
-{
-	void			*output;
-	unsigned int	total;
-
-	total = count * size;
-	output = malloc(total);
-	if (!output)
-		return (NULL);
-	ft_bzero(output, total);
-	return (output);
-}
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	count;
@@ -122,29 +83,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	return (NULL);
 }
-char	*ft_strdup(const char *src)
-{
-	int		i;
-	int		j;
-	char	*copy;
-
-	i = 0;
-	j = 0;
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	copy = (char *)malloc(i * sizeof(char) + 1);
-	if (!copy)
-		return (NULL);
-	while (j <= i)
-	{
-		copy[j] = src[j];
-		j++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
 
 static int	handle_remainder(char **container, char **remainder)
 {
@@ -170,7 +108,7 @@ static int	handle_remainder(char **container, char **remainder)
 			return 1;
 		}
 		i++;
-	}			
+	}
 	*container = ft_strjoin(*container, *remainder);
 	free(*remainder);
 	*remainder = NULL;
@@ -181,27 +119,29 @@ static char	*main_logic(int fd, char **container, char **remainder, char **buffe
 {
 	size_t chars;
 	size_t i;
-	while ((chars = read(fd, *buffer, BUFFER_SIZE)) > 0)  //tagliare qui, chars non serve come parametro
-	{
+
+	while ((chars = read(fd, *buffer, BUFFER_SIZE)) > 0)
+	 {
 		i = 0;
 		(*buffer)[chars] = '\0';
+
 		while (i < chars)
 		{
-			if ((*buffer)[i] == '\n')
-			{
+			if ((*buffer)[i] == '\n') {
 				*remainder = ft_substr(*buffer, i + 1, chars - i - 1);
-				*container = ft_strjoin(*container,  ft_substr(*buffer, 0, i + 1));
+				*container = ft_strjoin(*container, ft_substr(*buffer, 0, i + 1));
 				free(*buffer);
-				return (*container);
+				return *container;
 			}
 			i++;
 		}
-		*container = ft_strjoin(*container, *buffer);
-		free(*buffer);
-		return (*container);
-	}		
-}
 
+		*container = ft_strjoin(*container, *buffer);
+	}
+
+	free(*buffer);
+	return *container;
+}
 
 char *get_next_line(int fd)
 {
@@ -211,30 +151,11 @@ char *get_next_line(int fd)
 	size_t chars;
 	size_t i;
 
-	container = "";					//da chiarire
+	container = "";
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
-	if (remainder)
-		if (handle_remainder(&container, &remainder) == 1)
-			return (container);
-	while ((chars = read(fd, buffer, BUFFER_SIZE)) > 0)  //tagliare qui, chars non serve come parametro
-	{
-		i = 0;
-		buffer[chars] = '\0';
-		while (i < chars)
-		{
-			if (buffer[i] == '\n')
-			{
-				remainder = ft_substr(buffer, i + 1, chars - i - 1);
-				container = ft_strjoin(container,  ft_substr(buffer, 0, i + 1));
-				free(buffer);
-				return container;
-			}
-			i++;
-		}
-		container = ft_strjoin(container, buffer);
-	}
-	free(buffer);
-	return container;
+	if (remainder && (handle_remainder(&container, &remainder) == 1))
+		return container;
+	return main_logic(fd, &container, &remainder, &buffer);
 }
 
 
@@ -245,7 +166,7 @@ int main(void)
 	size_t	chars;
 
 	fd = open("test.txt", O_CREAT | O_RDONLY);	
-	for (size_t i = 0; i < 10; i++)	
+	for (size_t i = 0; i < 4; i++)	
 		printf("%s",get_next_line(fd));
 		/*puts("-------------------------");
 		printf("%s",get_next_line(fd));
